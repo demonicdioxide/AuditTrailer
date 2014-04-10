15ffd6927a1e5be60686d5db9dc6c2fccaa1c8a9
Arran <demonicdioxide@gmail.com>
2014-04-10 20:21:13 +0100

    Many improvements to reminder service

---
+++ AuditTrailer.Application/Model/ReminderResponse.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Arran
+ * Date: 10/04/2014
+ * Time: 19:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace AuditTrailer.Application.Model
+{
+	/// <summary>
+	/// Description of ReminderResponse.
+	/// </summary>
+	public class ReminderResponse
+	{
+		public ReminderResponse()
+		{
+		}
+	}
+}
